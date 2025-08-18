be7317c70ae3f689e916e16a4c5ca9fd32bc225b
MagicJinn <jinn.bz@mail.com>
2025-08-19 00:09:44 +0200

    Initial commit

---
+++ src/client/kotlin/magicjinn/beaconaura/BeaconAuraClient.kt
@@ -0,0 +1,9 @@
+package magicjinn.beaconaura
+
+import net.fabricmc.api.ClientModInitializer
+
+object BeaconAuraClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
